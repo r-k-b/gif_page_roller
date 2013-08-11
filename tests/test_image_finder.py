a66b4d5c3ca616970ee1f8c81acd6b1fdf353e33
rtzarius <rtzarius@gmail.com>
2013-08-11 18:57:24 +1000

    Add tests structure

---
+++ tests/test_image_finder.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import unittest
+
+class Test_image_finder(unittest.TestCase):
+    def test_gifs_are_found(self):
+        pass
+    
+    def test_non_gifs_are_ignored(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
