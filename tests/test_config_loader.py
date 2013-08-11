efe61add2b779c26d0ad4af637671cdbc39615b5
rtzarius <rtzarius@gmail.com>
2013-08-11 20:51:53 +1000

    First test for config loader
    TODO: fix relative file references breaking when imported from unexpected folder.

--- tests/test_config_loader.py
+++ tests/test_config_loader.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 import unittest
+import tools.conf_tools as conf_tools
 
 class Test_config_loader(unittest.TestCase):
     #.. TODO: implement these tests!
     
     def test_good_option_found(self):
-        return NotImplemented
+        config = conf_tools.Config_file(r'testdata\valid.yaml')
+        try:
+            optionvalue = config.read('count_to_three')
+        except Exception as e:
+            raise e
+        self.assertEqual(optionvalue, ['one', 'two', 'three'])
     
     def test_bad_option_found(self):
         return NotImplemented
